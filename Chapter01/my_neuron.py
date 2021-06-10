c87af8e7f03d105cafc2da50b98693f6187c1760
hestrang1993 <48037715+hestrang1993@users.noreply.github.com>
2021-06-09 23:37:18 -0400

    Started Work on Chapter 01
    Began work on Chapter 01 materials.
    Created and update following files:
    * requirements.txt
    ** >conda list -e > requirements.txt
    * tensorflow_2.yml
    ** >conda env export > tensorflow_2.yml
    * .gitignore (for Python)
    
    Harrison Strang

---
+++ Chapter01/my_neuron.py
@@ -0,0 +1,19 @@
+"""
+The :mod:`Chapter01.my_neuron` module contains the :class:`MyNeuron` class.
+
+The :class:`MyNeuron` class will be the building blocks for the neural network layers I want to create.
+"""
+
+
+# ==============================================================================
+# Imported Modules
+# ==============================================================================
+
+# ==============================================================================
+# Class Definition
+# ==============================================================================
+
+class MyNeuron:
+
+    def __init__(self):
+        pass
