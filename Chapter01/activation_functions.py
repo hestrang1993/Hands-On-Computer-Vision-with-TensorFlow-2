8144165443dd3011c2e617e345c5709ac829bafc
hestrang1993 <48037715+hestrang1993@users.noreply.github.com>
2021-06-10 22:31:54 -0400

    Worked on Chapter 01 Material
    Installed and set up JupyterLab.
    Added the tensorflow_2 conda environment to Jupyter as a kernel.
    Updated my_neuron.py.
    Began work on a Python file for activation functions.
    
    Harrison Strang

---
+++ Chapter01/activation_functions.py
@@ -0,0 +1,10 @@
+"""
+The :mod:`activation_functions` module will contain several activation functions and their derivatives.
+
+.. topic:: Activation Functions
+
+    This module will contain the following activation functions:
+
+    * The sigmoid function :math:`f\left(x\\right) = \\frac{1}{1 + e^{-x}}`
+    * The L2 loss function :math:`f\left(x\\right) =
+"""
