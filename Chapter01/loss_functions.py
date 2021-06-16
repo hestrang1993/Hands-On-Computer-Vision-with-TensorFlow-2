66f2500fdf6fc9b81ab838ac89e866f63a98177d
hestrang1993 <48037715+hestrang1993@users.noreply.github.com>
2021-06-16 17:36:42 -0400

    feat(chapter_01.loss_functions.py): add loss functions
    1. Add L1 loss function.
    2. Add L2 loss function.
    
    Harrison Strang

--- Chapter01/loss_functions.py
+++ Chapter01/loss_functions.py
@@ -6,6 +6,60 @@ The :mod:`loss_functions` module will contain several loss functions and their d
 # Imported Modules
 # ==============================================================================
 
+import numpy as np
+
+
 # ==============================================================================
-# Activation Functions
+# Loss Functions
 # ==============================================================================
+
+def l2_loss_function(y_predicted, y_true):
+    """
+    Use the L2 loss function to calculate the loss for a model.
+
+    .. math::
+        L_{2} \\left( y , y^{true} \\right) = \\sum_{i} { \\left( y_{i}^{true} - y_{i} \\right) }^2
+
+    Parameters
+    ----------
+    y_predicted : ndarray
+        The predicted output of the model.
+    y_true : ndarray
+        The actual output value.
+
+    Returns
+    -------
+    int or float
+        The value of the L2 loss function.
+    """
+    batch_size = y_predicted.shape[0]
+    square_difference = np.square(y_true - y_predicted)
+    sum_of_squares = np.sum(square_difference)
+    y = sum_of_squares / batch_size
+    return y
+
+
+def l1_loss_function(y_predicted, y_true):
+    """
+    Use the L1 loss function to calculate the loss for a model.
+
+    .. math::
+        L_{1} \\left( y , y^{true} \\right) = \\sum_{i} \\left| ( y_{i}^{true} - y_{i} \\right|
+
+    Parameters
+    ----------
+    y_predicted : ndarray
+        The predicted output of the model.
+    y_true : ndarray
+        The actual output value.
+
+    Returns
+    -------
+    int or float
+        The value of the L1 loss function.
+    """
+    batch_size = y_predicted.shape[0]
+    absolute_difference = np.abs(y_true - y_predicted)
+    sum_of_absolutes = np.sum(absolute_difference)
+    y = sum_of_absolutes / batch_size
+    return y
