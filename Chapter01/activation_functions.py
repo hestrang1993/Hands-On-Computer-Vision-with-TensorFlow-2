6fae7835470e91d14bcbe02981f25e70fce25f73
hestrang1993 <48037715+hestrang1993@users.noreply.github.com>
2021-06-16 17:16:56 -0400

    feat(chapter_01.activation_functions.py): add activation functions
    1. Add sigmoid activation function.
    2. Add derivative of sigmoid activation
    function.
    3. Add hyperbolic tangent activation function.
    4. Add derivative of hyperbolic tangent activation function.
    5. Add rectified linear unit (ReLU) activation function.
    6. Add derivative of rectified linear unit (ReLU) activation function.
    
    Harrison Strang

--- Chapter01/activation_functions.py
+++ Chapter01/activation_functions.py
@@ -1,16 +1,5 @@
 """
 The :mod:`activation_functions` module will contain several activation functions and their derivatives.
-
-.. topic:: Activation Functions
-
-    This module will contain the following activation functions:
-
-    * The sigmoid function :math:`S\left(y\\right) = \\frac{1}{1 + e^{-y}}`
-    * The L2 loss function :math:`L_{2} \left({y_{i}}^{true}, y_{i} \\right) = \sum{}_i \left( {y_{i}}^{true} - y_{i}
-    \\right)^2`
-    * The binary cross entropy function :math:`\\text{BCE} \left( {y_{i}}^{true} , {y_{i}} \\right ) = \sum_{i}
-    \left[ {-y_{i}}^{true} \log \left( y_{i} \\right) + \left( 1 - {y_{i}}^{true} \\right) \log \left( 1 - y_{i}
-    \\right ) \\right]`
 """
 
 # ==============================================================================
@@ -19,9 +8,8 @@ The :mod:`activation_functions` module will contain several activation functions
 
 import numpy as np
 
-
 # ==============================================================================
-# Function Definitions
+# Activation Functions
 # ==============================================================================
 
 def sigmoid_function(x):
@@ -29,7 +17,7 @@ def sigmoid_function(x):
     Use the sigmoid function as an activation function.
 
     .. math::
-        S\left(y\\right) = \\frac{1}{1 + e^{-y}}
+        S\left(x\\right) = \\frac{1}{1 + e^{-x}}
 
     Parameters
     ----------
@@ -49,18 +37,120 @@ def sigmoid_function(x):
 
 def derivative_of_sigmoid_function(x):
     """
-    The derivative of the sigmoid function.
+    Use the derivative of the sigmoid function.
 
     .. math::
-        \\frac{ \\mathrm{ dS } }{ \\mathrm{ d } y } = y \left( 1 - y \\right)
+        \\frac{ \\mathrm{ dS } }{ \\mathrm{ d } x } = y \left( 1 - x \\right)
 
     Parameters
     ----------
-    x
+    x : int or float
+        The value back-propagated from the proceeding layer.
 
     Returns
     -------
-
+    int or float
+        The value to back-propagate to the preceding layer.
     """
     y = x * (1 - x)
+    return y
+
+
+def hyperbolic_tangent_function(x):
+    """
+    Use the hyperbolic tangent function as an activation function.
+
+    .. math::
+        \\tanh\left(x\\right) = \\frac{ e^{ x } - e^{ -x } }{ e^{ x } + e^{ -x } }
+
+    .. NOTE::
+        This can also be calculated using ``return np.tanh(x)``.
+
+
+    Parameters
+    ----------
+    x : int or float
+        The weighted sum.
+
+    Returns
+    -------
+    float
+        The output of the neuron based on the hyperbolic tangent function.
+    """
+    a = np.exp(x)
+    b = np.exp(-x)
+    numerator = a - b
+    denominator = a + b
+    y = numerator / denominator
+    return y
+
+
+def derivative_of_hyperbolic_tangent_function(x):
+    """
+    Use the derivative of the hyperbolic tangent function.
+
+    .. math::
+        \\frac{ \\mathrm{ d } }{ \\mathrm{ d } x } \\tanh \\left( x \\right) = 1 - \\tanh^{2} \\left( x \\right)
+
+    Parameters
+    ----------
+    x : int or float
+        The value back-propagated from the proceeding layer.
+
+    Returns
+    -------
+    int or float
+        The value to back-propagate to the preceding layer.
+    """
+    y = 1 - np.tanh(x) ** 2
+    return y
+
+
+def rectified_linear_unit_function(x):
+    """
+    Use the rectified linear unit (ReLU) function.
+
+    .. math::
+        \\text{ReLU} \\left( x \\right) = \\begin{Bmatrix} 0 & \\text{if } x < 0 \\ x & \\text{if } x \\geq 0 \\end{
+        matrix}
+
+    Parameters
+    ----------
+    x : int or float
+        The weighted sum.
+
+    Returns
+    -------
+    int or float
+        The output of the neuron based on the ReLU activation function.
+    """
+    if x < 0.0:
+        y = 0.0
+    else:
+        y = x
+    return y
+
+
+def derivative_of_rectified_linear_unit_function(x):
+    """
+    Use the derivative of the rectified linear unit (ReLU) function.
+
+    .. math::
+        \\text{ReLU} \\left( x \\right) = \\begin{Bmatrix} 0 & \\text{if } x < 0 \\ 1 & \\text{if } x \\geq 0 \\end{
+        matrix}
+
+    Parameters
+    ----------
+    x : int or float
+        The weighted sum.
+
+    Returns
+    -------
+    int or float
+        The output of the neuron based on the ReLU activation function.
+    """
+    if x < 0:
+        y = 0.0
+    else:
+        y = 1.0
     return y
