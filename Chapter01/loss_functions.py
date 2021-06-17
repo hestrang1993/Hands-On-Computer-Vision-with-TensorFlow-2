10d4d962bd07b41d8f6b092c069e9d12eb157dbe
hestrang1993 <48037715+hestrang1993@users.noreply.github.com>
2021-06-16 20:27:27 -0400

    feat(chapter_01.my_fully_connected_layer.py): add fully connected layer
    1. Create a fully connected layer for test purposes.
    2. Document the fully connected layer.
    3. Test the fully connected layer.
    
    Harrison Strang

--- Chapter01/loss_functions.py
+++ Chapter01/loss_functions.py
@@ -63,3 +63,77 @@ def l1_loss_function(y_predicted, y_true):
     sum_of_absolutes = np.sum(absolute_difference)
     y = sum_of_absolutes / batch_size
     return y
+
+
+def binary_cross_entropy_loss_function(y_predicted, y_true):
+    """
+    Use the binary cross entropy (BCE) loss function for a model.
+
+    .. math::
+        BCE \\left( y_{i}^{true} , y_{i} \\right) = \\sum_{i} \\left( -y_{i}^{true} \\log \\left( y_{i} \\right) +
+        \\left( 1 - y_{i}^{true} \\right) \\log \\left( 1 - y_{i} \\right) \\right)
+
+    Parameters
+    ----------
+    y_predicted : ndarray
+        The predicted output of the model.
+    y_true : ndarray
+        The actual output value.
+
+    Returns
+    -------
+    int or float
+        The value of the L1 loss function.
+    """
+    a = np.multiply(y_true, np.log(y_predicted))
+    b = np.multiply(np.log(1 - y_predicted), (1 - y_true))
+    y = -np.mean(a + b)
+    return y
+
+
+# ==============================================================================
+# Derivatives of the Loss Functions
+# ==============================================================================
+
+def derivative_of_l2_loss_function(y_predicted, y_true):
+    """
+    Use the derivative of the L2 loss function.
+
+    Parameters
+    ----------
+    y_predicted : ndarray
+        The predicted output of the model.
+    y_true : ndarray
+        The actual output value.
+
+    Returns
+    -------
+    int or float
+        The derivative of the L2 loss function.
+    """
+    y = 2 * (y_predicted - y_true)
+    return y
+
+
+def derivative_of_binary_cross_entropy_loss_function(y_predicted, y_true):
+    """
+    Use the derivative of the binary cross entropy (BCE) loss function.
+
+    Parameters
+    ----------
+    y_predicted : ndarray
+        The predicted output of the model.
+    y_true : ndarray
+        The actual output value.
+
+    Returns
+    -------
+    int or float
+        The derivative of the BCE loss function.
+    """
+    numerator = (y_predicted - y_true)
+    denominator = (y_predicted * (1 - y_predicted))
+    y = numerator / denominator
+    return y
+
+# TODO: Add derivative to the L1 loss function.
